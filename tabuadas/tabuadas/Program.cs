dffbdaeb04f5a3842af7993005b2150bd2de6c1b
Giuliano <giuliano.phocus@gmail.com>
2020-02-11 16:13:33 -0300

    projeto criado

---
+++ tabuadas/tabuadas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuadas {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um número: ");
+
+            int n;
+            int r;
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 11; i++) {
+                r = n * i;
+                Console.WriteLine(i + "x" + n + "=" + r);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
